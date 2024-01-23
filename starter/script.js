04bae2c77830a41f581087ba7a420c6c02df1aab
NadineTsianta <k.tsianta@rocketmail.com>
2024-01-23 17:06:28 +0000

    made changes in the user submit | I have it as two seperate functions instead of just one

--- starter/script.js
+++ starter/script.js
@@ -9,26 +9,36 @@ let timeBlockEl = $('.time-block')
 $('#currentDay').text(today.format("dddd D, MMMM"));
 
 
-
-
-
-function userSubmit() {
+// Retrieve stored data on page load
+function loadStoredData() {
     userInputEl.each(function () {
         let userInput = $(this);
-        let timeBlock = userInput.closest('.row').find('.time-block').text(); //Find  the closest parent with the class .row, then finds the child with the class. To ensure that I get the correct time block associated with the user input.
-        let key = today.format('YYYY-MM-DD') + ' ' + timeBlock; //Create a key using the date . To ensure that each entry has a unique identifier.
-        let toDo = userInput.val();
-        let storedData = localStorage.getItem(key); //to retrieve stored data
+        let timeBlock = userInput.closest('.row').find('.time-block').text();
+        let key = today.format('YYYY-MM-DD') + ' ' + timeBlock;
+        let storedData = localStorage.getItem(key);
 
         if (storedData) {
-            // If there is stored data, update the input value
             userInput.val(storedData);
         }
-
-        // Store the user input in local storage
-        localStorage.setItem(key, toDo);
     });
-};
+}
+
+// Call the function to load stored data
+loadStoredData();
+
+
+
+function userSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    userInputEl.each(function () {
+        let userInput = $(this);
+        let timeBlock = userInput.closest('.row').find('.time-block').text();//Find  the closest parent with the class .row, then finds the child with the class. To ensure that I get the correct time block associated with the user input.
+        let key = today.format('YYYY-MM-DD') + ' ' + timeBlock; //Create a key using the date . To ensure that each entry has a unique identifier.
+        let toDo = userInput.val();
+        localStorage.setItem(key, toDo); //to retrieve stored data
+    });
+}
 
 saveBtnEl.on('click', userSubmit)
 
