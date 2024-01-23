d807c6d8efc870447569f9053f18b4699b9449b4
NadineTsianta <k.tsianta@rocketmail.com>
2024-01-23 16:17:47 +0000

    made changes in the function for the user input and where to store data

--- starter/script.js
+++ starter/script.js
@@ -15,21 +15,27 @@ $('#currentDay').text(today.format("dddd D, MMMM"));
 function userSubmit() {
     userInputEl.each(function () {
         let userInput = $(this);
-        let storedData = localStorage.getItem(userInput.attr('id'));
+        let timeBlock = userInput.closest('.row').find('.time-block').text(); //Find  the closest parent with the class .row, then finds the child with the class. To ensure that I get the correct time block associated with the user input.
+        let key = today.format('YYYY-MM-DD') + ' ' + timeBlock; //Create a key using the date . To ensure that each entry has a unique identifier.
+        let toDo = userInput.val();
+        let storedData = localStorage.getItem(key); //to retrieve stored data
 
         if (storedData) {
             // If there is stored data, update the input value
             userInput.val(storedData);
         }
+
+        // Store the user input in local storage
+        localStorage.setItem(key, toDo);
     });
 }
-
 
 
 //to change the colour of the timeblocks
 
 function updateColours() {
 
+    let currentHour = today.hour();
     
     timeBlockEl.each(function () {
         let timeBlock = $(this);
@@ -44,18 +50,18 @@ function updateColours() {
             timeBlock.addClass('future').removeClass('past present');
         }
     });
-    let relativeTime = require('dayjs/plugin/relativeTime')
-    dayjs.extend(relativeTime)
     
-    let time = dayjs.extend(relativeTime);
-    dayjs().from(dayjs('1990-01-01')) // in 31 years
-    dayjs().from(dayjs('1990-01-01'), true) // 31 years
-    dayjs().fromNow()
-    
-    dayjs().to(dayjs('1990-01-01')) // "31 years ago"
-    dayjs().toNow()
 }
 
 updateColours();
 
-setInterval(updateColours, 60000);
+
+
+setInterval(function (){
+    let newHour = dayjs().hour();
+    if (newHour !== today.hour()) {
+        today =dayjs();
+        $('#currentDay').text(today.format("dddd D, MMMM"))
+        updateColours()
+    }
+}, 60000);
