f1b3fab9f4e33a0b880f06e0266cb52326bb908c
NadineTsianta <k.tsianta@rocketmail.com>
2024-01-23 15:36:54 +0000

    added function to compare the time in order to change colour of the list items

--- starter/script.js
+++ starter/script.js
@@ -1,19 +1,38 @@
+let today = dayjs();
+let time = dayjs.extend(relativeTime);
+let userInputEl = $('.description');
+let saveBtnEl = $('.saveBtn');
+let timeBlockEl = $('.time-block')
 
-var calendar = require('dayjs/plugin/calendar')
-dayjs.extend(calendar)
 
-dayjs().calendar(dayjs('2008-01-01'))
-dayjs().calendar(null, {
-  sameDay: '[Today at] h:mm A', // The same day ( Today at 2:30 AM )
-  nextDay: '[Tomorrow at] h:mm A', // The next day ( Tomorrow at 2:30 AM )
-  nextWeek: 'dddd [at] h:mm A', // The next week ( Sunday at 2:30 AM )
-  lastDay: '[Yesterday at] h:mm A', // The day before ( Yesterday at 2:30 AM )
-  lastWeek: '[Last] dddd [at] h:mm A', // Last week ( Last Monday at 2:30 AM )
-  sameElse: 'DD/MM/YYYY' // Everything else ( 17/10/2011 )
-})
 
-$( function() {
-    $( "#selectable" ).selectable();
-  } );
+$('#currentDay').text(today.format("dddd D, MMMM"));
 
-  
+let userSubmit = function (event) {
+    event.preventDefault();
+
+    let toDo = userInputEl.val();
+};
+
+
+saveBtnEl.on('click', userSubmit);
+
+function updateColours() {
+    timeBlockEl.each(function () {
+        let timeBlock = $(this);
+        let timeText = timeBlock.find('.time').text();
+        let blockTime = dayjs(timeText, 'hh A');
+
+        if (blockTime.isBefore(today, 'hour')) {
+            timeBlock.addClass('past').removeClass('present future');
+        } else if (blockTime.isSame(today, 'hour')) {
+            timeBlock.addClass('present').removeClass('past future');
+        } else {
+            timeBlock.addClass('future').removeClass('past present');
+        }
+    });
+}
+
+updateColours();
+
+setInterval(updateColours, 60000);
